fb5570b7b6f53ae5cd95dc38dc2f31e84e46f45f
bnnlukas <bonn_lukas@gmx.de>
2023-05-29 19:58:42 +0200

    Text classification with BERT

--- functions.py
+++ functions.py
@@ -1,7 +1,64 @@
+import pickle
+import torch
+import numpy as np
+from transformers import BertTokenizer, BertForSequenceClassification
+
+tokenizer = BertTokenizer.from_pretrained(
+    'bert-base-uncased',
+    do_lower_case = True
+    )
+
+classes_mapping = {
+    'business': 0,
+    'entertainment': 1,
+    'politics': 2,
+    'sport': 3,
+    'tech': 4
+}
+
 def text_summary(user_text, compression_rate):
     summary = 'Das ist eine Testzusammenfassung'
     return summary
 
+from transformers import BertTokenizer
+
+def preprocessing(input_text, tokenizer):
+  return tokenizer.encode_plus(
+                        input_text,
+                        add_special_tokens = True,
+                        max_length = 32,
+                        pad_to_max_length = True,
+                        return_attention_mask = True,
+                        return_tensors = 'pt'
+                   )
+
+
 def text_classification(user_text):
-    text_class = 'Business'
-    return text_class
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # load the model from disk
+    filename = './models/finalized_model.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))
+
+    # We need Token IDs and Attention Mask for inference on the new sentence
+    test_ids = []
+    test_attention_mask = []
+
+    # Apply the tokenizer
+    encoding = preprocessing(user_text, tokenizer)
+
+    # Extract IDs and Attention Mask
+    test_ids.append(encoding['input_ids'])
+    test_attention_mask.append(encoding['attention_mask'])
+    test_ids = torch.cat(test_ids, dim = 0)
+    test_attention_mask = torch.cat(test_attention_mask, dim = 0)
+
+    # Forward pass, calculate logit predictions
+    with torch.no_grad():
+        output = loaded_model(test_ids.to(device), token_type_ids = None, attention_mask = test_attention_mask.to(device))
+
+    keys = [k for k, v in classes_mapping.items() if v == np.argmax(output.logits.cpu().numpy()).flatten().item()]
+    predicted_class = keys[0]
+
+    return predicted_class
